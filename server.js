a420f8d62d3edad342971877bc4f41a2b512923f
wikiwaleed2 <wikiwaleed2@gmail.com>
2021-11-14 03:00:45 +0500

    Signed-off-by: wikiwaleed2 <wikiwaleed2@gmail.com>

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 import express from 'express';
 import { createServer } from 'http';
+
 import { ApolloServer } from 'apollo-server-express';
+
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var privateKey  = fs.readFileSync('C:\\Certbot\\live\\dmapi.walztech.com\\privkey.pem', 'utf8');
+var certificate = fs.readFileSync('C:\\Certbot\\live\\dmapi.walztech.com\\fullchain.pem', 'utf8');
+var credentials = {key: privateKey, cert: certificate};
+
 
 import typeDefs  from './data/schema';
 import resolvers from './data/resolvers';
@@ -12,10 +21,20 @@ const app = express();
 const apolloServer = new ApolloServer({ typeDefs, resolvers });
 apolloServer.applyMiddleware({ app });
 
-const httpServer = createServer(app);
-apolloServer.installSubscriptionHandlers(httpServer);
+const httpsServer = https.createServer(credentials, app);
+apolloServer.installSubscriptionHandlers(httpsServer);
 
-httpServer.listen({ port: PORT }, () =>{
-  console.log(`🚀 Server ready at http://localhost:${PORT}${apolloServer.graphqlPath}`)
-  console.log(`🚀 Subscriptions ready at ws://localhost:${PORT}${apolloServer.subscriptionsPath}`)
+httpsServer.listen({ port: PORT }, () =>{
+  console.log(`🚀 Server ready at https://localhost:${PORT}${apolloServer.graphqlPath}`)
+  console.log(`🚀 Subscriptions ready at wss://localhost:${PORT}${apolloServer.subscriptionsPath}`)
 })
+
+// var httpServer = http.createServer(app);
+// var httpsServer = https.createServer(credentials, app);
+
+// httpServer.listen(4080, () => {
+  // console.log("HTTP server starting on port : " + 4080)
+// });
+// httpsServer.listen(4000, () => {
+  // console.log("HTTP'S' server starting on port : " + 4000)
+// });
